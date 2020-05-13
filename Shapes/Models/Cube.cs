b39172582fd58400dcce45aef3da1fe67ea38527
Steven Ly <1stevenly92@gmail.com>
2020-05-13 09:17:55 -0700

    did basic layout

---
+++ Shapes/Models/Cube.cs
@@ -0,0 +1,11 @@
+namespace Shapes.Models
+{
+    public class Cube
+    {
+        public Rectangle Face {get; set;}
+        public Cube (Rectangle side)
+        {
+            Face = side;
+        }
+    }
+}
